d8ef88bead2f9943c7feb2c59b922e0e6493e7db
陈益超 <348294078@qq.com>
2023-05-18 14:25:53 +0800

    u

--- app/Support/CustomClient.php
+++ app/Support/CustomClient.php
@@ -4,31 +4,47 @@ namespace App\Support;
 
 use App\Support\Traits\InstanceMake;
 use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
 
 abstract class CustomClient
 {
     use InstanceMake;
 
     protected Client $client;
+
     public function __construct()
     {
-        $this->client = new Client();
+        $this->client = new Client([
+            'base_uri' => $this->baseUri(),
+            'timeout' => $this->timeout()
+        ]);
     }
 
-    abstract protected function baseUri();
+    abstract protected function baseUri(): string;
 
     protected function timeout(): int
     {
         return config('support.custom_client.timeout', 10);
     }
 
-    public function request(string $url, array $data, string $method, array $options, array $header)
+    protected function request(string $method, string $url, array $options)
     {
-//        $this->client->
-//        $this->client->request('');
+        $response = $this->client->request($method, $url, $options);
+
+        return $this->handleResponse($response);
     }
 
-    protected function handleResponse()
+    protected function get(string $url, array $options)
+    {
+        $this->request('GET', $url, $options);
+    }
+
+    protected function post(string $url, array $options)
+    {
+        $this->request('POST', $url, $options);
+    }
+
+    protected function handleResponse(ResponseInterface $response):mixed
     {
 
     }
